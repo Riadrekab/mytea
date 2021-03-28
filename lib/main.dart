57a863c1652ef34b228c93c123cb5dd34a6dddb1
Riad Rekab <riad.rekab@kepler-technologies.com>
2021-03-28 14:47:33 +0100

    prevent screen from rotating

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mytea/mainPage.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         theme: ThemeData(
           fontFamily: "Kam",
