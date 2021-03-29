ea1a92d89c9f6cbc7e5a94d8041d64b10bb8d517
Riad Rekab <riad.rekab@kepler-technologies.com>
2021-03-29 15:30:07 +0100

    Adding dismiss action for deleting products from cart

--- lib/screens/panier.dart
+++ lib/screens/panier.dart
@@ -5,45 +5,69 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:matcher/matcher.dart';
 import 'package:mytea/Widgets/ProductTitle.dart';
 import 'package:mytea/Widgets/Produit.dart';
+import 'package:mytea/Widgets/ProduitPan.dart';
 
 class Panier extends StatefulWidget {
   _PanState createState() => _PanState();
 }
 
 class _PanState extends State<Panier> {
+  List<ProduitPanCard> list = [
+    ProduitPanCard(
+      img: true,
+      imgL: "assets/images/tea.png",
+      nomProduit: "Thé vert",
+      prix: "40Da",
+    ),
+    ProduitPanCard(
+      img: true,
+      imgL: "assets/images/croissant.png",
+      nomProduit: "Thé vert + miel",
+      prix: "50Da",
+    ),
+    ProduitPanCard(
+      img: true,
+      imgL: "assets/images/fraise.png",
+      nomProduit: "Grand Thé vert + miel",
+      prix: "70Da",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     double WidthS = MediaQuery.of(context).size.width;
     double HeightS = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: ListView(
+        body: ListView.builder(
           shrinkWrap: true,
           scrollDirection: Axis.vertical,
-          children: [
-            SizedBox(height: 6),
-            ProduitCard(
-              img: true,
-              imgL: "assets/images/tea.png",
-              nomProduit: "Thé vert",
-              prix: "40Da",
-            ),
-            SizedBox(height: 6),
-            ProduitCard(
-              img: true,
-              imgL: "assets/images/croissant.png",
-              nomProduit: "Thé vert + miel",
-              prix: "50Da",
-            ),
-            SizedBox(height: 6),
-            ProduitCard(
-              img: true,
-              imgL: "assets/images/fraise.png",
-              nomProduit: "Grand Thé vert + miel",
-              prix: "70Da",
-            ),
-            SizedBox(height: 6),
-          ],
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int i) {
+            final item = list[i].toStringShort();
+            return list.length > 0
+                ? Column(
+                    children: [
+                      SizedBox(height: 6),
+                      Dismissible(
+                        background: Container(
+                          color: Colors.red,
+                          child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Supprimer",
+                                style: TextStyle(fontSize: 30),
+                              )),
+                        ),
+                        key: Key(item),
+                        onDismissed: (direction) {
+                          list.removeAt(i);
+                        },
+                        child: list[i],
+                      ),
+                    ],
+                  )
+                : Center();
+          },
         ),
       ),
     );
